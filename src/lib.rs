816647d9cb40dcdc6471a45ecccd7dc3f402515f
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2025-03-29 11:07:23 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![cfg_attr(feature = "experimental_allocator", feature(allocator_api))]
 
+#[cfg(any(feature = "std_alloc", test))]
+extern crate std;
+
 pub mod flex_array;
 pub mod types;
