70f58d6ef4c2aaf1c1cd3d7ab93fe27f4c454c07
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2025-03-29 00:27:21 -0700

    Update array.rs

--- src/flex_array/array.rs
+++ src/flex_array/array.rs
@@ -1,13 +1,44 @@
+use core::alloc::Layout;
+use core::marker::PhantomData;
+use core::mem::align_of;
+
 use super::inner::Inner;
 use crate::types::AltAllocator;
+use crate::types::FlexArrResult;
 use crate::types::LengthType;
-use core::marker::PhantomData;
 
 pub struct FlexArr<T, L: LengthType, A: AltAllocator>
 where
     usize: TryFrom<L>,
 {
     inner: Inner<L, A>,
-    len: L,
-    _ph: PhantomData<T>,
+    len:   L,
+    _ph:   PhantomData<T>,
+}
+
+impl<T, L: LengthType, A: AltAllocator> FlexArr<T, L, A>
+where
+    usize: TryFrom<L>,
+{
+    pub const fn new_in(alloc: A) -> Self {
+        return Self {
+            inner: Inner::new_in(alloc, align_of::<T>()),
+            len:   L::ZERO_VALUE,
+            _ph:   PhantomData,
+        };
+    }
+
+    pub fn with_capacity(alloc: A, capacity: L) -> FlexArrResult<Self> {
+        let lay = Layout::new::<T>();
+        let inner = Inner::initial_alloc(alloc, capacity, lay)?;
+        return Ok(Self {
+            inner: inner,
+            len:   L::ZERO_VALUE,
+            _ph:   PhantomData,
+        });
+    }
+
+    pub const fn capacity(&self) -> L {
+        return self.inner.capacity(size_of::<T>());
+    }
 }
