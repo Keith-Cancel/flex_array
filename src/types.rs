4c14f8ef959c5d99d920ad4bf8d8ae3a23e9fead
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2025-03-29 12:35:41 -0700

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,11 @@
 mod alt_alloc;
 mod errors;
 mod len_type;
+#[cfg(feature = "std_alloc")]
+mod std_alloc;
 
 pub use alt_alloc::AltAllocator;
 pub use errors::*;
 pub use len_type::LengthType;
+#[cfg(feature = "std_alloc")]
+pub use std_alloc::StdAlloc;
