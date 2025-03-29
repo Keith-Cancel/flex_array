f74a34e65b5816a35cb4bf00346630ca4f7828e3
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2025-03-28 22:16:41 -0700

    Add Length Type Trait for working using other types as a length, capacity and index type.

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,7 @@
 mod alt_alloc;
+mod len_type;
 
 pub use alt_alloc::AllocError;
 pub use alt_alloc::AltAllocator;
+
+pub use len_type::LengthType;
---
+++ src/types/len_type.rs
@@ -0,0 +1,75 @@
+use core::cmp;
+use core::ops;
+
+/// This trait is used for letting you specify the type of the length and
+/// capacity fields of a flex array container, As well as indexing operations.
+/// If you have some other type that you want to use and behaves like an unsigned integer,
+/// you can implement this trait for it.
+///
+/// It's marked as unsafe since your type must be continuous and ordered.
+/// under common operations such as addition multiplication like integers.
+///
+pub unsafe trait LengthType
+where
+    Self: Copy,
+    Self: cmp::Eq,
+    Self: cmp::Ord,
+    Self: cmp::PartialEq,
+    Self: cmp::PartialOrd,
+    Self: ops::Add<Output = Self>,
+    Self: ops::AddAssign,
+    Self: ops::Mul<Output = Self>,
+    Self: ops::MulAssign,
+    Self: ops::Sub<Output = Self>,
+    Self: ops::SubAssign,
+    Self: Sized,
+    usize: TryFrom<Self>,
+{
+    const MIN_VALUE: Self;
+    const MAX_VALUE: Self;
+    fn checked_add(self, rhs: Self) -> Option<Self>;
+    fn checked_sub(self, rhs: Self) -> Option<Self>;
+    fn checked_mul(self, rhs: Self) -> Option<Self>;
+    fn checked_div(self, rhs: Self) -> Option<Self>;
+    fn checked_rem(self, rhs: Self) -> Option<Self>;
+    fn count_ones(self) -> u32;
+}
+
+macro_rules! impl_length_type {
+    ($typ:ty) => {
+        unsafe impl LengthType for $typ {
+            const MIN_VALUE: Self = Self::MIN;
+            const MAX_VALUE: Self = Self::MAX;
+            #[inline]
+            fn checked_add(self, rhs: Self) -> Option<Self> {
+                return self.checked_add(rhs);
+            }
+            #[inline]
+            fn checked_sub(self, rhs: Self) -> Option<Self> {
+                return self.checked_sub(rhs);
+            }
+            #[inline]
+            fn checked_mul(self, rhs: Self) -> Option<Self> {
+                return self.checked_mul(rhs);
+            }
+            #[inline]
+            fn checked_div(self, rhs: Self) -> Option<Self> {
+                return self.checked_div(rhs);
+            }
+            #[inline]
+            fn checked_rem(self, rhs: Self) -> Option<Self> {
+                return self.checked_rem(rhs);
+            }
+            #[inline]
+            fn count_ones(self) -> u32 {
+                return self.count_ones();
+            }
+        }
+    };
+}
+
+impl_length_type!(usize);
+impl_length_type!(u8);
+impl_length_type!(u16);
+impl_length_type!(u32);
+impl_length_type!(u64);
