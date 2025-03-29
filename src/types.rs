790b4c53865c5f7227eeaed4441458fff7c66170
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2025-03-28 22:55:47 -0700

    Move Errors to there own file.

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,8 @@
 mod alt_alloc;
+mod errors;
 mod len_type;
 
-pub use alt_alloc::AllocError;
 pub use alt_alloc::AltAllocator;
+pub use errors::*;
 
 pub use len_type::LengthType;
--- src/types/alt_alloc.rs
+++ src/types/alt_alloc.rs
@@ -1,28 +1,6 @@
+use super::AllocError;
 use core::alloc::Layout;
 use core::ptr::NonNull;
-
-#[cfg(feature = "experimental_allocator")]
-pub use core::alloc::AllocError;
-#[cfg(feature = "experimental_allocator")]
-use core::alloc::Allocator;
-
-/// This indicates some sort of memory allocation error for the alt allocator.
-///
-/// If the rust allocator API is enabled this will be the same error type as
-/// the Allocator API.
-#[cfg(not(feature = "experimental_allocator"))]
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct AllocError;
-
-#[cfg(not(feature = "experimental_allocator"))]
-impl core::error::Error for AllocError {}
-
-#[cfg(not(feature = "experimental_allocator"))]
-impl core::fmt::Display for AllocError {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.write_str("A memory allocation error occurred.")
-    }
-}
 
 /// The rust allocator API is not stable yet. Therefore, this trait
 /// can be used to implement/wrap a custom allocator in a no_std environment.
