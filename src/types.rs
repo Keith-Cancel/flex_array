af3d50961929902d6d996343d0ff9e40a8406355
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2025-03-29 14:35:34 -0700

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,12 @@
+//! Contains mainly traits used by `FlexArr` The important types are the `LengthType`
+//! trait and the `AltAllocator` trait.
+//!
+//! If the `experimental_allocator` feature is enabled, the `AltAllocator`
+//! trait is implemented for types that implement the allocator api `Allocator` trait.
+//!
+//! If built with the `std_alloc` feature, a wrapper called `Global` is also
+//! provided. Further, if the `experimental_allocator` feature is enabled,
+//! the allocator APIs `Global` is re-exported instead.
 mod alt_alloc;
 mod errors;
 mod len_type;
