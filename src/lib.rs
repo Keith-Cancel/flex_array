773c4225d98021db930741f1d6189e0637bda1cd
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2025-03-29 17:28:20 -0700

    Reorder this and and change the wording a tad.

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,13 @@
 //!
 //! I wrote `FlexArr` to address some limitations of Rust’s standard `std::Vec`:
 //!
-//! - **Fallible Allocations**: Instead of panicking on allocation failure, `FlexArr` returns an
-//!   error, allowing for more robust or graceful error handling. While `std::Vec` has some fallible
-//!   methods, most are still unstable.
-//!
 //! - **Reduced Memory Overhead**: The `std::Vec` on a 64-bit system typically uses 24 bytes.
 //!   By specifying a smaller type for length and capacity (e.g., `u32`), `FlexArr` can reduce
 //!   this to just 16 bytes, optimizing memory usage.
+//!
+//! - **Fallible Allocations**: Instead of panicking on allocation failure, `FlexArr` returns an
+//!   error, allowing for more robust or graceful error handling. While `std::Vec` has some fallible
+//!   methods, most are still unstable.
 //!
 //! - **Custom Allocator Support**: Since Rust’s allocator API is unstable, `FlexArr` introduces
 //!   the `AltAllocator` trait as an alternative to the standard `Allocator` trait. If the the
@@ -22,12 +22,12 @@
 //!
 //! ## Feature Flags
 //!
-//! - `std_alloc` – Enables a `Global` wrapper that implements `AltAllocator` using the standard
+//! - `std_alloc` – Enables a wrapper called `Global` that implements `AltAllocator` using the standard
 //!   allocator APIs.
 //!
 //! - `experimental_allocator` – Enables support for Rust’s unstable `Allocator` trait for custom
 //!   allocators. When used with `std_alloc`, this re-exports the `Global` type from `std` instead
-//!   of the custom `Global` wrapper.
+//!   of the custom wrapper named `Global`.
 #![no_std]
 #![cfg_attr(feature = "experimental_allocator", feature(allocator_api))]
 
