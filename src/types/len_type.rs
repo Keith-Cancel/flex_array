e1dcf523fe8b0827ac435d8f242624d4146aacef
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2025-03-30 22:41:28 -0700

    I don't use multiply atm so I don't need this.

--- src/types/len_type.rs
+++ src/types/len_type.rs
@@ -41,8 +41,6 @@ where
     fn checked_add(self, rhs: Self) -> Option<Self>;
     /// The same as `checked_sub` for rust's built in types.
     fn checked_sub(self, rhs: Self) -> Option<Self>;
-    /// The same as `checked_mul` for rust's built in types.
-    fn checked_mul(self, rhs: Self) -> Option<Self>;
     /// The same as `wrapping_add` for rust's built in types.
     fn wrapping_add(self, rhs: Self) -> Self;
 }
@@ -68,11 +66,6 @@ macro_rules! impl_length_type {
             #[inline]
             fn checked_sub(self, rhs: Self) -> Option<Self> {
                 return self.checked_sub(rhs);
-            }
-
-            #[inline]
-            fn checked_mul(self, rhs: Self) -> Option<Self> {
-                return self.checked_mul(rhs);
             }
 
             #[inline(always)]
