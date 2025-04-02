878f692248c828b8169c291c7257ca2e5be429b2
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2025-04-02 11:35:09 -0700

    This error is now in the alloc module.

--- src/types/errors.rs
+++ src/types/errors.rs
@@ -1,25 +1,5 @@
-#[cfg(feature = "experimental_allocator")]
-pub use core::alloc::AllocError;
 use core::error::Error;
 use core::fmt;
-
-/// This indicates some sort of memory allocation error for the alt allocator.
-///
-/// If the rust allocator API is enabled this will be the same error type as
-/// the Allocator API.
-#[cfg(not(feature = "experimental_allocator"))]
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct AllocError;
-
-#[cfg(not(feature = "experimental_allocator"))]
-impl Error for AllocError {}
-
-#[cfg(not(feature = "experimental_allocator"))]
-impl fmt::Display for AllocError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("A memory allocation error occurred.")
-    }
-}
 
 /// This enum lets one figure out what the reason an error occurred durning
 /// a `FlexArr` operation.
