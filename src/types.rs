3e5b307fce92924311d6a57f22440d62b1f6e78c
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2025-04-02 11:35:42 -0700

    Remove the moved types.

--- src/types.rs
+++ src/types.rs
@@ -7,14 +7,8 @@
 //! If built with the `std_alloc` feature, a wrapper called `Global` is also
 //! provided. Further, if the `experimental_allocator` feature is enabled,
 //! the allocator APIs `Global` is re-exported instead.
-mod alt_alloc;
 mod errors;
 mod len_type;
-#[cfg(feature = "std_alloc")]
-mod std_alloc;
 
-pub use alt_alloc::AltAllocator;
 pub use errors::*;
 pub use len_type::LengthType;
-#[cfg(feature = "std_alloc")]
-pub use std_alloc::Global;
